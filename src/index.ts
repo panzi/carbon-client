a9168a4dc3b8e63a5362a4c6d6a5b30defc6eb6f
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2022-04-02 05:47:32 +0200

    handle concurrent modifications of event handlers

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,10 @@ const PATH_REGEXP = /^[-_a-zA-Z0-9]+(\.[-_a-zA-Z0-9]+)*$/;
 const TAG_NAME_REGEXP = PATH_REGEXP;
 const TAG_VALUE_REGEXP = /^[-_.,:\/%+a-zA-Z0-9]*$/;
 
+type Arg0<F extends Function> =
+    F extends (arg0: infer T) => unknown ? T :
+    F extends () => unknown ? void : never;
+
 export type IPTransport = 'UDP'|'TCP';
 export type Transport = IPTransport|'IPC';
 
@@ -54,6 +58,17 @@ function appendTags(buf: string[]|(string|number)[], tags: Tags): void {
     }
 }
 
+interface CallbackInfo<Callback extends Function> {
+    callback: Callback;
+    once: boolean;
+}
+
+interface Callbacks<Callback extends Function> {
+    callbacks: CallbackInfo<Callback>[];
+    remove: Callback[];
+    emitActive: boolean;
+}
+
 export class CarbonClient {
     readonly address: string;
     readonly port: number;
@@ -62,10 +77,10 @@ export class CarbonClient {
     autoReconnect: boolean;
 
     private socket: NetSocket|DgramSocket|null = null;
-    private callbacks: { [key in keyof EventMap]: EventMap[key][] } = {
-        connect: [],
-        error:   [],
-        close:   [],
+    private callbacks: { [key in keyof EventMap]: Callbacks<EventMap[key]> } = {
+        connect: { callbacks: [], emitActive: false, remove: [] },
+        error:   { callbacks: [], emitActive: false, remove: [] },
+        close:   { callbacks: [], emitActive: false, remove: [] },
     };
 
     constructor(address: string, port?: number, transport?: IPTransport, autoReconnect?: boolean);
@@ -107,28 +122,26 @@ export class CarbonClient {
     }
 
     private _onConnect = () => {
-        for (const callback of this.callbacks.connect) {
-            callback.call(this);
-        }
+        this._emit('connect', undefined);
     };
 
     private _onError = (error: Error) => {
-        for (const callback of this.callbacks.error) {
-            callback.call(this, error);
-        }
+        this._emit('error', error);
     };
 
     private _onClose = (hadError: boolean) => {
         try {
-            for (const callback of this.callbacks.close) {
-                callback.call(this, hadError);
-            }
+            this._emit('close', hadError);
         } finally {
             try {
                 if (this.socket) {
                     this.socket.off('connect', this._onConnect);
                     this.socket.off('error', this._onError);
-                    this.socket.off('close', this._onClose);
+                    if (this.transport === 'TCP' || this.transport === 'IPC') {
+                        this.socket.off('close', this._onClose);
+                    } else {
+                        this.socket.off('close', this._onCloseOk);
+                    }
                 }
             } finally {
                 this.socket = null;
@@ -137,6 +150,10 @@ export class CarbonClient {
                 }
             }
         }
+    };
+
+    private _onCloseOk = () => {
+        this._onClose(false);
     };
 
     async connect(): Promise<void> {
@@ -162,7 +179,7 @@ export class CarbonClient {
                 sendBufferSize: this.sendBufferSize,
             });
 
-            this.socket.on('close', () => this._onClose(false));
+            this.socket.on('close', this._onCloseOk);
         }
 
         this.socket.on('connect', this._onConnect);
@@ -253,14 +270,60 @@ export class CarbonClient {
     }
 
     on<Event extends keyof EventMap>(event: Event, callback: EventMap[Event]): void {
-        this.callbacks[event].push(callback);
+        this.callbacks[event].callbacks.push({ callback, once: false });
+    }
+
+    once<Event extends keyof EventMap>(event: Event, callback: EventMap[Event]): void {
+        this.callbacks[event].callbacks.push({ callback, once: true });
     }
 
     off<Event extends keyof EventMap>(event: Event, callback: EventMap[Event]): void {
-        const callbacks = this.callbacks[event];
-        const index = callbacks.indexOf(callback);
-        if (index >= 0) {
-            callbacks.splice(index, 1);
+        const infos = this.callbacks[event];
+        // avoid concurrent modification
+        if (infos.emitActive) {
+            infos.remove.push(callback);
+        } else {
+            const index = infos.callbacks.findIndex(info => info.callback === callback);
+            if (index >= 0) {
+                infos.callbacks.splice(index, 1);
+            }
+        }
+    }
+
+    private _emit<Event extends keyof EventMap>(event: Event, arg: Arg0<EventMap[Event]>): void {
+        const infos = this.callbacks[event];
+        const oldEmitActive = infos.emitActive;
+        infos.emitActive = true;
+        try {
+            for (const info of infos.callbacks) {
+                try {
+                    info.callback.call(this, arg);
+                } catch (error) {
+                    if (event !== 'error' && this.callbacks.error.callbacks.length > 0) {
+                        setImmediate(() =>
+                            this._emit(
+                                'error',
+                                error instanceof Error ? error : new Error(String(error))));
+                    } else {
+                        throw error;
+                    }
+                } finally {
+                    if (info.once) {
+                        infos.remove.push(info.callback);
+                    }
+                }
+            }
+        } finally {
+            infos.emitActive = oldEmitActive;
+            if (!oldEmitActive && infos.remove.length > 0) {
+                for (const callback of infos.remove) {
+                    const index = infos.callbacks.findIndex(info => info.callback === callback);
+                    if (index >= 0) {
+                        infos.callbacks.splice(index, 1);
+                    }
+                }
+                infos.remove = [];
+            }
         }
     }
 
@@ -391,5 +454,16 @@ export class CarbonClient {
         const data = buf.join('');
 
         return this._send(data);
+    }
+
+    vwrite(path: string, value: number, timestamp: Date, tags?: Tags): void;
+    vwrite(path: string, value: number, tags?: Tags): void;
+
+    vwrite(path: string, value: number, arg3?: Date|Tags, arg4?: Tags): void {
+        this.write(path, value, arg3 as any, arg4).catch(this._onError);
+    }
+
+    vbatchWrite(batch: MetricMap|MetricTuple[], timestamp?: Date): void {
+        this.batchWrite(batch, timestamp).catch(this._onError);
     }
 }
